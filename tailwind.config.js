dadeaea47a4a28cd535c5f1c183bc0f9db617dab
Bill Nagbe <BillyNagbe@gmail.com>
2025-01-21 07:40:25 -0600

    starting grid tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      purple100: "#dacffc",
+      purple500: "#7650dc",
+      yellow100: "#f9eee1",
+      yellow500: "#ffcb6b",
+      whitepure: "#fff",
+      black50: "#121212"
+    },
+
+    fontSize: {
+      fontBase: "18px",
+      fontSmall: "9px",
+      h1: "18px"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+

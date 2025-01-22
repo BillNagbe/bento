baab43f06f75665a6340030f55d2938530935d39
Bill Nagbe <BillyNagbe@gmail.com>
2025-01-22 13:31:28 -0600

    finished mobile design tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      purple100: "#dacffc",
-      purple500: "#7650dc",
-      yellow100: "#f9eee1",
-      yellow500: "#ffcb6b",
-      whitepure: "#fff",
-      black50: "#121212"
-    },
     extend: {
       fontSize: {
         fontBase: "18px",
-        fontSmall: "0.4rem",
-        fontsemibold: "0.5rem",
-        h1: "18px"
+        "small-screen": "9px",
+        "small-screen-h1": "10px",
+        "small-screen-p": "6px"
+      },
+      colors: {
+        purple100: "#dacffc",
+        purple500: "#7650dc",
+        yellow100: "#f9eee1",
+        yellow500: "#ffcb6b",
+        whitepure: "#fff",
+        black50: "#121212"
       }
     },
   },
